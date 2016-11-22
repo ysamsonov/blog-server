cdf1e4f590d6240b3f0ca261fddb3caf93278827
Yuriy Samsonov <yuriy.samsonov96@gmail.com>
2016-11-22 19:15:04 +0300

    Add methods to edit/delete comment

--- src/main/java/me/academeg/api/rest/CommentController.java
+++ src/main/java/me/academeg/api/rest/CommentController.java
@@ -1,14 +1,18 @@
 package me.academeg.api.rest;
 
+import me.academeg.entity.Account;
 import me.academeg.entity.Article;
 import me.academeg.entity.Comment;
+import me.academeg.exceptions.AccountPermissionException;
 import me.academeg.exceptions.ArticleNotExistException;
 import me.academeg.exceptions.CommentNotExistException;
 import me.academeg.exceptions.EmptyFieldException;
+import me.academeg.security.Role;
 import me.academeg.service.AccountService;
 import me.academeg.service.ArticleService;
 import me.academeg.service.CommentService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.validation.annotation.Validated;
@@ -73,12 +77,44 @@ public class CommentController {
     public Comment edit(@PathVariable UUID uuid,
                         @RequestBody Comment commentRequest,
                         @AuthenticationPrincipal User user) {
-        return null;
+
+        if (commentRequest.getText() == null) {
+            throw new EmptyFieldException();
+        }
+
+        Comment commentFromDb = commentService.getByUuid(uuid);
+        if (commentFromDb == null) {
+            throw new CommentNotExistException();
+        }
+
+        Account account = accountService.getByEmail(user.getUsername());
+
+        if (!commentFromDb.getAuthor().getId().equals(account.getId())) {
+            throw new AccountPermissionException();
+        }
+
+        commentFromDb.setText(commentRequest.getText());
+        return commentService.edit(commentFromDb);
     }
 
     @RequestMapping(value = "/{uuid}", method = RequestMethod.DELETE)
+    @ResponseStatus(value = HttpStatus.NO_CONTENT)
     public void delete(@PathVariable UUID uuid,
                        @AuthenticationPrincipal User user) {
 
+        Comment commentFromDb = commentService.getByUuid(uuid);
+        if (commentFromDb == null) {
+            throw new CommentNotExistException();
+        }
+
+        Account account = accountService.getByEmail(user.getUsername());
+
+        if (!(commentFromDb.getAuthor().getId().equals(account.getId())
+                || account.getAuthority().equals(Role.ROLE_MODERATOR.name())
+                || account.getAuthority().equals(Role.ROLE_ADMIN.name()))) {
+            throw new AccountPermissionException();
+        }
+
+        commentService.delete(commentFromDb);
     }
 }
