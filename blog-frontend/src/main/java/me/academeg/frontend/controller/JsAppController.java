baba5f0018fdeb82eb3c995b801e7dfadf44e61c
Yuriy Samsonov <yuriy.samsonov96@gmail.com>
2016-12-01 10:16:03 +0300

    Some fixes

--- blog-frontend/src/main/java/me/academeg/frontend/controller/JsAppController.java
+++ blog-frontend/src/main/java/me/academeg/frontend/controller/JsAppController.java
@@ -1,6 +1,5 @@
-package me.academeg.frontend;
+package me.academeg.frontend.controller;
 
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
