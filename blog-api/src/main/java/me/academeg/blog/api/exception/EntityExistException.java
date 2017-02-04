91a7a5cb9321ba01033c636cfdcb5ab3f5971740
Yuriy Samsonov <yuriy.samsonov96@gmail.com>
2017-02-04 13:12:03 +0300

    create base exception class

--- blog-api/src/main/java/me/academeg/blog/api/exception/EntityExistException.java
+++ blog-api/src/main/java/me/academeg/blog/api/exception/EntityExistException.java
@@ -10,13 +10,17 @@ import org.springframework.web.bind.annotation.ResponseStatus;
  * @version 1.0
  */
 @ResponseStatus(value = HttpStatus.BAD_REQUEST)
-public class EntityExistException extends RuntimeException {
+public class EntityExistException extends BlogException {
 
     public EntityExistException(String message) {
         super(message);
     }
 
     public EntityExistException(String message, Object... args) {
-        super(String.format(message, args));
+        super(message, args);
+    }
+
+    public EntityExistException(Throwable cause) {
+        super(cause);
     }
 }
