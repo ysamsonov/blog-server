62ed182b3498ba62e4cdd9e4f015c59d3a455e65
Yuriy Samsonov <yuriy.samsonov96@gmail.com>
2017-02-07 21:35:42 +0300

    refs #15 add tests for BaseEntiy

--- blog-api/src/test/java/me/academeg/blog/dal/domain/BaseEntityTest.java
+++ blog-api/src/test/java/me/academeg/blog/dal/domain/BaseEntityTest.java
@@ -34,6 +34,35 @@ public class BaseEntityTest {
         assertThat(entity.getId()).isEqualTo(id);
     }
 
+    @Test
+    public void hashTest() throws Exception {
+        UUID id = UUID.randomUUID();
+        TestEntity entity = new TestEntity(id);
+
+        assertThat(entity.hashCode()).isEqualTo(id.hashCode());
+    }
+
+    @Test
+    public void equalsAndHashCodeTest1() throws Exception {
+        String id = "aacb6ad3-5465-4097-982f-5d1496ff34a1";
+        TestEntity entity1 = new TestEntity(UUID.fromString(id));
+        TestEntity entity2 = new TestEntity(UUID.fromString(id));
+
+        assertThat(entity1.hashCode()==entity2.hashCode()).isTrue();
+        assertThat(entity1.equals(entity2)).isTrue();
+    }
+
+    @Test
+    public void equalsAndHashCodeTest2() throws Exception {
+        String id1 = "aacb6ad3-5465-4097-982f-5d1496ff34a1";
+        String id2 = "aacb6ad3-5465-4097-982f-5d1496ff34a2";
+        TestEntity entity1 = new TestEntity(UUID.fromString(id1));
+        TestEntity entity2 = new TestEntity(UUID.fromString(id2));
+
+        assertThat(entity1.hashCode()==entity2.hashCode()).isFalse();
+        assertThat(entity1.equals(entity2)).isFalse();
+    }
+
     static class TestEntity extends BaseEntity {
         public TestEntity() {
         }
