5fd70805801573b0ca053fe01cf8f8e1d7e51986
Yuriy Samsonov <yuriy.samsonov96@gmail.com>
2017-03-28 17:54:16 +0300

    all service now transactional

--- blog-api/src/main/java/me/academeg/blog/dal/service/impl/CommentServiceImpl.java
+++ blog-api/src/main/java/me/academeg/blog/dal/service/impl/CommentServiceImpl.java
@@ -11,6 +11,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Date;
 import java.util.Optional;
@@ -23,6 +24,7 @@ import java.util.UUID;
  * @version 1.0
  */
 @Service
+@Transactional
 public class CommentServiceImpl implements CommentService {
 
     private final ArticleService articleService;
