e6062449d88dab4cde52527b2313932ac28ef744
Yuriy Samsonov <yuriy.samsonov96@gmail.com>
2017-03-28 20:23:39 +0300

    enable cors filter by special profile

---
+++ blog-api/src/main/java/me/academeg/blog/Profiles.java
@@ -0,0 +1,9 @@
+package me.academeg.blog;
+
+/**
+ * @author Yuriy A. Samsonov <y.samsonov@erpscan.com>
+ * @since 28.03.2017
+ */
+public final class Profiles {
+    public final static String CLIENT_INTEGRATION = "client-integration";
+}
